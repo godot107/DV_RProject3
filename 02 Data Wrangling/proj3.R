3801c5453a55a81d57ebe568bd13713877cc76a1
Willie Man <man.willieman@yahoo.com>
2015-03-03 13:56:53 -0600

    no message

--- 02 Data Wrangling/proj3.R
+++ 02 Data Wrangling/proj3.R
@@ -1,8 +1,33 @@
 require("jsonlite")
 library("RCurl")
+require(dplyr)
 
 BTC <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from BTC"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
 
 GDP <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from GDP"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
 
 EXCHANGE <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from EXCHANGE"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+
+GDP20 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from GDP20"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+
+EXCHANGE20 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from TOP20"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+
+colnames(GDP20) <- toupper(names(GDP20))
+colnames(EXCHANGE20) <- toupper(names(EXCHANGE20))
+
+colnames(GDP) <- toupper(names(GDP))
+colnames(EXCHANGE) <- toupper(names(EXCHANGE))
+
+
+# 10 
+head(BTC)
+head(GDP)
+head(EXCHANGE)
+
+# 11
+inner_join(EXCHANGE20, GDP20, by = "COUNTRY")
+
+#outer attempt
+inner_join(EXCHANGE, GDP, by = "COUNTRY")
+left_join(EXCHANGE,GDP, by = "COUNTRY")
+full_join(EXCHANGE,GDP, by = "COUNTRY")
