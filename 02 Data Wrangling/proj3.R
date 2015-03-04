73e64348fe96885f3599d844e123ac75d82d73d3
Willie Man <man.willieman@yahoo.com>
2015-03-04 13:14:02 -0600

    attempted to add a full join and attempted to plot it. but still need to fix its presentation.

--- 02 Data Wrangling/proj3.R
+++ 02 Data Wrangling/proj3.R
@@ -38,13 +38,14 @@ inner_join(EXCHANGE20, GDP20, by = "COUNTRY")
 >>>>>>> 1212e9a271df0b04bfe7cef6fbd76e024cdd5689
 
 #outer attempt
-full_join(BIRTH, POP, by = "COUNTRY") %>% tbl_df()
+full <- full_join(BIRTH, POP, by = "COUNTRY") %>% tbl_df()
 # join climate and arable percentages?.
 
 #12 some interesting plots
 ggplot(joined, aes(x=EXCHANGEPERDOLLAR, y = GDP, color = RANKING)) + geom_point()
 
-
+#this is very beautiful... could we try to average birthrates of regions? like in a group by to make it cleaner. group by REGION AND PRESENT AVERAGE BIRTHRATE?
+ggplot(full, aes(x=COUNTRY, y = BIRTHRATE, color = POPULATION)) + geom_point()
 
 #13
 
@@ -55,6 +56,4 @@ ggplot(joined, aes(x = GDP)) + geom_histogram(binwidth=100000) + labs(x="GDP of 
 #categorical # APPARENTLY not enough categorical variables; we only have one and that is region
 categoricals <- eval(parse(text=substring(getURL(URLencode('http://129.152.144.84:5001/rest/native/?query="select * from WORLD "'), httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDB1.usuniversi01134.oraclecloud.internal',  USER='C##cs329e_wkm285', PASS='orcl_wkm285', MODE='native_mode', MODEL='model', returnFor = 'R', returnDimensions = 'True'), verbose = TRUE), 1, 2^31-1)))
 
-
-#png("/Users/pcannata/Mine/UT/GitRepositories/DataVisualization/RWorkshop/00 Doc/categoricals.png", width = 25, height = 10, units = "in", res = 72)
 
