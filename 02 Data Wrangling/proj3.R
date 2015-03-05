55eef9535e386d5ec7e0489574f6e43c90269477
Lauren Wendler <lwendler@utexas.edu>
2015-03-04 18:00:14 -0600

    no message

--- 02 Data Wrangling/proj3.R
+++ 02 Data Wrangling/proj3.R
@@ -4,6 +4,8 @@ require(dplyr)
 require("ggplot2")
 library(extrafont)
 
+#9 Creating Data Frames 
+
 GDP <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from GDP"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_lar2478', PASS='orcl_lar2478',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
 
 CTR <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from COUNTRY"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_lar2478', PASS='orcl_lar2478',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
@@ -26,33 +28,31 @@ BIRTH <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?
 colnames(GDP20) <- toupper(names(GDP20))
 colnames(EXCHANGE20) <- toupper(names(EXCHANGE20))
 
-# 10 
+# 10 subset of data frames
 head(GDP)
 head(EXCHANGE)
 head(WORLD)
 
-# 11
-<<<<<<< HEAD
+# 11 Inner-join by country 
 joined <- inner_join(EXCHANGE20, GDP20, by = "COUNTRY")
-=======
 inner_join(EXCHANGE20, GDP20, by = "COUNTRY")
->>>>>>> 1212e9a271df0b04bfe7cef6fbd76e024cdd5689
 
-#outer attempt
+#outer join by country
 full <- full_join(BIRTH, POP, by = "COUNTRY") %>% tbl_df()
 
+# join from combine data sets column
 
-# join climate and arable percentages?.
+#12 visualizations of wrangled data sets
+# we are curious if there's a relationship between the value of a country's currency compared to the US as a gold standard and the country's economy measured by GDP. Visualization of Inner Join
+ggplot(joined, aes(x=EXCHANGEPERDOLLAR, y = GDP, color = RANKING)) + geom_point() scale_color_gradient(low="darkkhaki",high="darkgreen") + theme(plot.title = element_text(size=20, face = "bold" , vjust=2)) + theme(axis.text.x=element_text(angle=50, size=20, vjust=0.5)) + theme(legend.title = element_text(colour = black, size = 16, face = "bold")) scale_color_discrete(name = "Ranking") + theme(legend.key=element_rect(fill='pink'))
 
-#12 some interesting plots
-# we are curious if there's a relationship between the value of a country's currency compared to the US as a gold standard and the country's economy measured by GDP
-ggplot(joined, aes(x=EXCHANGEPERDOLLAR, y = GDP, color = RANKING)) + geom_point() + ggtitle('US EXCHANGE RATE VS GDP')
-
+#Visualization of Outer join
 ggplot(full, aes(x=COUNTRY, y = BIRTHRATE, color = POPULATION)) + geom_point()
 full %>% group_by(REGION.x, BIRTHRATE) %>% summarise(mean_br=mean(BIRTHRATE)) %>% ggplot(aes(x = REGION.x, y=mean_br)) + geom_point() + ggtitle('BIRTHRATES OF PER REGION') + theme(legend.title=element_text(colour="chocolate",size=16,face="bold")) + theme(axis.test.x=element_text(angle=50, size = 20, vjust=.5))
 
-#13
+#Visualization of Combined Data set join 
 
+#13 PNG Image
 #histogram,
 #Exchange Rate of curreny 
 ggplot(joined, aes(x = GDP)) + geom_histogram(binwidth=100000) + labs(x="GDP in Dollars ") + ggtitle('GDP of Countries')
