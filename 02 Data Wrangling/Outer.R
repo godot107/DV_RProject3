5659589615198010d2bab4c36d4930356540e021
Lauren Wendler <lwendler@utexas.edu>
2015-03-06 21:37:55 -0600

    no message

--- 02 Data Wrangling/Outer.R
+++ 02 Data Wrangling/Outer.R
@@ -1,10 +1,16 @@
 #Data Frames Used:
 POP <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from POP"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
 
+head(POP)
+
 BIRTH <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from BIRTH"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_wkm285', PASS='orcl_wkm285',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE))) 
 
-#Outer Join
+head(BIRTH)
+
+#Outer Join by country
 full <- full_join(BIRTH, POP, by = "COUNTRY") %>% tbl_df()
+
+head(full)
 
 #Visualization of Outer Join
 full %>% group_by(REGION.x, BIRTHRATE) %>% summarise(mean_br=mean(BIRTHRATE)) %>% ggplot(aes(x = REGION.x, y=mean_br)) + geom_point() + ggtitle('BIRTHRATES OF PER REGION') + theme(legend.title=element_text(colour="chocolate",size=16,face="bold")) + theme(axis.text.x=element_text(angle=90, size=5, vjust=0.5)) + xlab("Regions around the world") + ylab("mean birth rate / population rate")
--- 02 Data Wrangling/combine.R
+++ 02 Data Wrangling/combine.R
@@ -6,7 +6,16 @@ library(extrafont)
 
 
 FULLGDP <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from FULLGDP"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_lar2478', PASS='orcl_lar2478',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+
+head(FULLGDP)
+
 LIT <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from LIT"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_lar2478', PASS='orcl_lar2478',MODE='native_mode',MODEL='model',returnDimensions = 'False',returnFor = 'JSON'),verbose = TRUE)))
+
+head(LIT)
+
+#Combine join by country 
 left<- left_join(LIT, FULLGDP, by = "COUNTRY")
+
+head(left)
 
 left%>% select(COUNTRY,REGION, LITERACY, GDP) %>% ggplot(aes(x=LITERACY, y=GDP, color=REGION)) + geom_point()
