087cafb2b2b9c09838e47b7149d185fabfa995dc
Lauren Wendler <lwendler@utexas.edu>
2015-03-06 19:53:14 -0600

    no message

--- 02 Data Wrangling/Inner.R
+++ 02 Data Wrangling/Inner.R
@@ -13,4 +13,4 @@ EXCHANGE20 <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/nat
 joined <- inner_join(EXCHANGE20, GDP20, by = "COUNTRY")
 
 #Visualization of Inner Join
-ggplot(joined, aes(x=EXCHANGEPERDOLLAR, y = GDP, color = RANKING)) + geom_point() + scale_color_gradient(low="darkkhaki",high="darkgreen") + theme(plot.title = element_text(size=20, face = "bold" , vjust=2)) + theme(axis.text.x=element_text(angle=50, size=20, vjust=0.5))  
+ggplot(joined, aes(x=EXCHANGEPERDOLLAR, y = GDP)) + geom_jitter(alpha=1,aes(color=RANKING),position=position_jitter(width=.3) + scale_color_gradient(low="darkkhaki",high="darkgreen") + ggtitle('GDP vs. Exchange Per US Dollar') + theme(plot.title = element_text(size=20, face = "bold" , vjust=2)) + theme(axis.text.x=element_text(angle=50, size=20, vjust=0.5))  
