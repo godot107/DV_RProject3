ceb1cf0e684c842c46e736d5b707d15bbbb8b201
Lauren Wendler <lwendler@utexas.edu>
2015-03-06 21:50:43 -0600

    no message

--- 02 Data Wrangling/combine.R
+++ 02 Data Wrangling/combine.R
@@ -18,4 +18,4 @@ left<- left_join(LIT, FULLGDP, by = "COUNTRY")
 
 head(left)
 
-left%>% select(COUNTRY,REGION, LITERACY, GDP) %>% ggplot(aes(x=LITERACY, y=GDP, color=REGION)) + geom_point() + ggtitle('Literacy vs. GDP') + theme(legend.title=element_text(colour="chocolate",size=16,face="bold"))
+left%>% select(COUNTRY,REGION, LITERACY, GDP) %>% ggplot(aes(x=LITERACY, y=GDP, color=REGION)) + geom_point() + ggtitle('Literacy vs. GDP') + theme(legend.title=element_text(colour="chocolate",size=16,face="bold")) 
